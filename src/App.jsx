5dde648070171b8ad8bcdd683832649ec921a2ac
Sheyla <sheylapdv@gmail.com>
2023-07-03 14:11:27 +0200

    Avatar

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+function App() {
+  return (
+    <article>
+      <header>
+        <img
+          alt="El avatar de Sheyla"
+          src="https://unavatar.io/twitter/kikobeats"
+        />
+        <div>
+          <strong>Sheyla Pérez del Valle</strong>
+          <span>@sheylapdv</span>
+        </div>
+      </header>
+
+      <aside>
+        <button>Seguir</button>
+      </aside>
+    </article>
+  );
+}
+
+export default App;
